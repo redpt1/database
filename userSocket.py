73cc2ca58d896737f38a4dc9203811eff032bfb7
redpt1 <397240017@qq.com>
2021-04-24 15:11:35 +0800

    完成了 用户管理模块

---
+++ userSocket.py
@@ -0,0 +1,10 @@
+import socket
+import sys
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 信息传输socket
+seInfoSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #验证信息端口
+
+
+
+
+
